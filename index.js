8f894ab3e703d43ee1509584fb4735de613a0cc7
rayapu776-ui <rayapu776@gmail.com>
2026-08-06 00:02:37 +0530

    modifie

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+import dotenv from "dotenv"
+
+
+dotenv.config({
+    path : "./env"
+});
+
+let myusername = process.env.database
+
+console.log("Value: ",myusername)
+
 console.log("Start of backend project");
+
