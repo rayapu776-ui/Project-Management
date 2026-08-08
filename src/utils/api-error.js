2e6e76b544f65b01222270dadee1d36f39ad82c3
rayapu776-ui <rayapu776@gmail.com>
2026-08-08 15:51:58 +0530

    add new all system

---
+++ src/utils/api-error.js
@@ -0,0 +1,21 @@
+class ApiError extends Errors {
+    constructor(statusCode , message = "Something went wrong",
+        error = [],
+        stack = " "
+    ) {
+        super(message)
+        this.statusCode = statusCode
+        this.data = this.data
+        this.message = message
+        this.success = false
+        this.errors = errors
+
+        if(stack){
+            this.stack = stack
+        } else {
+            Error.captureStackTrace(this,this.constructor)
+        }
+    }
+}
+
+export {ApiError};
